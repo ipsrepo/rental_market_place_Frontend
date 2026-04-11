3b8892b0fc8b551c0a739477d74045af0e680b17
Prakash <prakaships@outlook.com>
2026-04-11 12:35:02 +0100

    - Refactor property service

--- src/services/property.service.js
+++ src/services/property.service.js
@@ -1,23 +1,25 @@
 import axiosInstance from "./API/axiosInstance.js";
 import {API_ENDPOINTS} from "../constants/endPoints.js";
 
+const API = API_ENDPOINTS.PROPERTY;
 
-export const getAllProperties = () => axiosInstance.get(API_ENDPOINTS.PROPERTY);
+
+export const getAllProperties = () => axiosInstance.get(API);
 
 export const addProperty = (formData) =>
-    axiosInstance.post(API_ENDPOINTS.PROPERTY, formData, {
+    axiosInstance.post(API, formData, {
         headers: {'Content-Type': 'multipart/form-data'},
     });
 
 export const getPropertyById = (id) =>
-    axiosInstance.get(`${API_ENDPOINTS.PROPERTY}/${id}`)
+    axiosInstance.get(`${API}/${id}`)
 
 export const updateProperty = (id, formData) =>
-    axiosInstance.patch(`${API_ENDPOINTS.PROPERTY}/${id}`, formData, {
+    axiosInstance.patch(`${API}/${id}`, formData, {
         headers: {'Content-Type': 'multipart/form-data'},
     });
 
-export const deleteProperty = (id) => axiosInstance.delete(`${API_ENDPOINTS.PROPERTY}/${id}`);
+export const deleteProperty = (id) => axiosInstance.delete(`${API}/${id}`);
 
 export const getUserProperties = (ownerId) =>
-    axiosInstance.get(`${API_ENDPOINTS.PROPERTY}/owner/${ownerId}`);
+    axiosInstance.get(`${API}/owner/${ownerId}`);
